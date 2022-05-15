ccb98d735a636a67bb6109d24f0e40a256c79e72
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-15 10:44:39 +0530

    converted into blob format, printed all layer names

--- detect.py
+++ detect.py
@@ -1,5 +1,10 @@
 import cv2
 
+##############
+# Parameters
+w_h_target = 320
+###############
+
 cap = cv2.VideoCapture(0)
 
 classNames = []
@@ -13,10 +18,17 @@ modelWeights = 'yolov3.weights'
 net = cv2.dnn.readNet(modelCfg, modelWeights)
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-print(net)
 
 while True:
     success, img = cap.read()
+    blob = cv2.dnn.blobFromImage(
+        img, 1 / 255, (w_h_target, w_h_target),
+        [0, 0, 0], 1, crop=False
+    )
+    net.setInput(blob)
+
+    layerNames = net.getLayerNames()
+    print(layerNames)
 
     cv2.imshow("Web-cam", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
