cf17f24650034c08c72cc38d0b6764195bfce4c2
NASEEM A P <naseemalassampattil@gmail.com>
2022-05-15 10:38:43 +0530

    loaded network using Darknet by cfg and weight file

--- detect.py
+++ detect.py
@@ -5,7 +5,15 @@ cap = cv2.VideoCapture(0)
 classNames = []
 with open('coco.names', 'rt') as f:
     classNames = f.read().rstrip('\n').rsplit('\n')
-print(classNames)
+# print(classNames)
+
+modelCfg = 'yolov3.cfg'
+modelWeights = 'yolov3.weights'
+
+net = cv2.dnn.readNet(modelCfg, modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+print(net)
 
 while True:
     success, img = cap.read()
